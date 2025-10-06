5666b14f0e3737522a86b32488deecbea9c9ecf5
ArpitY01 <ay.arpit.yadav1@gmail.com>
2025-10-06 14:08:31 +0530

    Initial NetBeans JavaFX project commit

---
+++ src/main/java/com/mycompany/cloud_compass/Cloud_Compass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cloud_compass;
+
+/**
+ *
+ * @author Arpit
+ */
+public class Cloud_Compass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
